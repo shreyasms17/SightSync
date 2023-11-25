011fd42e2f21dc89ec660379496fa92f4cf5695f
Shreyas Mondikal Subramanya <shreyasms98@gmail.com>
2023-11-24 19:11:15 -0800

    Towhee Video feature extraction

--- src/frame_extraction/main.py
+++ src/frame_extraction/main.py
@@ -11,6 +11,6 @@ database_config = yaml.safe_load(open("database_config.yml"))
 type = "input" if ((len(sys.argv)==2 and sys.argv[1]=="input") or len(sys.argv)==1) else "query"
 
 ext_object = Extractor(input_config, database_config, type)
-ext_object.extract_scene_changing_frames()
-if (len(sys.argv)==4 and sys.argv[3]=="input") or len(sys.argv) < 4:
-    ext_object.extract_and_index_features()
+ext_object.extract_visual_frames()
+if type == "input":
+    ext_object.extract_and_index_visual_features()
