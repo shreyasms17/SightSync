42801bcc7cdc86cb894eb6a2802938a651c0934d
Bhargav <bhargavauden1998@gmail.com>
2023-12-07 08:41:45 +0530

    voting done

--- src/search_engine/selector.py
+++ src/search_engine/selector.py
@@ -1,20 +1,10 @@
 import sys
-import os
 import yaml
 
 sys.path.append("src")
-import subprocess
 
-import zmq
-import pickle
-from pymilvus import db, connections, Collection
-from towhee import ops, pipe, DataCollection
-from glob import glob
-from utils.time_utils import time_it
-from feature_extraction.extractor import Extractor
 import pandas as pd
 
-from multiprocessing import Process, Queue
 
 
 class Selector:
@@ -22,8 +12,7 @@ class Selector:
     def __init__(self):
         self.video_metadata = yaml.safe_load(open("config/video_metadata.yml"))
 
-    def select_best_match(self, audio_result, video_result):
-
+    def createVideoDataframe(self, video_result):
         vid_search_res_list = []
         for i, d in enumerate(video_result.to_list()):
             search_res = d['search_res']
@@ -33,8 +22,9 @@ class Selector:
                 vid_search_res_list.append(item)
 
         # Create Pandas DataFrame
-        video_df = pd.DataFrame(vid_search_res_list, columns=['hitid', 'score', 'media_id', 'start', 'second'])
+        return pd.DataFrame(vid_search_res_list, columns=['hitid', 'score', 'media_id', 'start', 'second'])
 
+    def createAudioDataFrame(self, audio_result):
         audio_search_res_list = []
         for i, d in enumerate(audio_result):
             search_res = d[2]
@@ -43,26 +33,40 @@ class Selector:
                 formatted_item = [item[0], item[1], item[2].split('/')[-1], item[-1], i]
                 audio_search_res_list.append(formatted_item)
 
-        # Create Pandas DataFrame
-        audio_df = pd.DataFrame(audio_search_res_list, columns=['hitid', 'score', 'media_id', 'start', 'second'])
+        return pd.DataFrame(audio_search_res_list, columns=['hitid', 'score', 'media_id', 'start', 'second'])
 
+    def select_best_match(self, audio_result, video_result):
+
+        video_df = self.createVideoDataframe(video_result)
+        audio_df = self.createAudioDataFrame(audio_result)
+
+        # Get the most popular candidate of both
         vd_most_frequent_value = video_df['media_id'].value_counts().idxmax()
         aud_most_frequent_value = audio_df['media_id'].value_counts().idxmax()
 
         if vd_most_frequent_value == aud_most_frequent_value:
             vid_set = set(video_df[video_df['second'] == 0]['start'].tolist())
             aud_set = set(audio_df[audio_df['second'] == 0]['start'].tolist())
-            print(vid_set, aud_set)
+            # print(video_df[video_df['second'] == 0][['start', 'score']], audio_df[audio_df['second'] == 0][['start', 'score']])
             common = vid_set.intersection(aud_set)
             if len(common) > 0:
                 return {"media_id": vd_most_frequent_value,
                         "time": sorted(list(common))[0]-1,
-                        "media_player_time": sorted(list(common))[0]/self.video_metadata[vd_most_frequent_value]['total_length']}
+                        "media_player_time": sorted(list(common))[0]/self.video_metadata[vd_most_frequent_value]['total_length'],
+                        "frame": (sorted(list(common))[0]-1) * 30,
+                        "videoHigh": (video_df[video_df['second'] == 0].iloc[0]['media_id'], video_df[video_df['second'] == 0].iloc[0]['start']),
+                        "audioHigh": (audio_df[audio_df['second'] == 0].iloc[0]['media_id'], audio_df[audio_df['second'] == 0].iloc[0]['start'])}
             else:
                 # return the earliest time
                 return {"media_id": vd_most_frequent_value,
                         "time": sorted(list(vid_set.union(aud_set)))[0]-1,
-                        "media_player_time": sorted(list(vid_set.union(aud_set)))[0]/self.video_metadata[vd_most_frequent_value]['total_length']}
+                        "media_player_time": sorted(list(vid_set.union(aud_set)))[0]/self.video_metadata[vd_most_frequent_value]['total_length'],
+                        "frame": (sorted(list(vid_set.union(aud_set)))[0]-1) * 30,
+                        "videoHigh": (video_df[video_df['second'] == 0].iloc[0]['media_id'],
+                                      video_df[video_df['second'] == 0].iloc[0]['start']),
+                        "audioHigh": (audio_df[audio_df['second'] == 0].iloc[0]['media_id'],
+                                      audio_df[audio_df['second'] == 0].iloc[0]['start'])
+                }
         else:
             video_grouped = video_df.groupby(['media_id', 'second'])['start'].apply(set).reset_index()
             audio_grouped = audio_df.groupby(['media_id', 'second'])['start'].apply(set).reset_index()
@@ -84,8 +88,19 @@ class Selector:
                                                       (video_grouped['second'].isin(common_seconds))]['start'])
                     return {"media_id": media_id,
                             "time": sorted(list(common_frames))[0]-1,
-                            "media_player_time": sorted(list(common_frames))[0]/self.video_metadata[video_grouped['media_id']]['total_length']}
+                            "media_player_time": sorted(list(common_frames))[0]/self.video_metadata[video_grouped['media_id']]['total_length'],
+                            "frame": (sorted(list(common_frames))[0]-1) * 30,
+                            "videoHigh": (video_df[video_df['second'] == 0].iloc[0]['media_id'],
+                                          video_df[video_df['second'] == 0].iloc[0]['start']),
+                            "audioHigh": (audio_df[audio_df['second'] == 0].iloc[0]['media_id'],
+                                          audio_df[audio_df['second'] == 0].iloc[0]['start'])
+                            }
 
             return {"media_id": video_df.iloc[0]['media_id'],
                     "time": video_df.iloc[0]['start']-1,
-                    "media_player_time": video_df.iloc[0]['start']/self.video_metadata[video_df.iloc[0]['media_id']]['total_length']}
+                    "media_player_time": video_df.iloc[0]['start']/self.video_metadata[video_df.iloc[0]['media_id']]['total_length'],
+                    "frame": (video_df.iloc[0]['start']-1) * 30,
+                    "videoHigh": (video_df[video_df['second'] == 0].iloc[0]['media_id'],
+                                  video_df[video_df['second'] == 0].iloc[0]['start']),
+                    "audioHigh": (audio_df[audio_df['second'] == 0].iloc[0]['media_id'],
+                                  audio_df[audio_df['second'] == 0].iloc[0]['start'])}
--- testing.py
+++ testing.py
@@ -9,25 +9,25 @@ from os.path import isfile, join
 
 onlyfiles = [(f.split('.')[0], f.split('.')[0].split('_')[0], f.split('.')[0].split('_')[1]) for f in listdir("data/testing/query_video") if isfile(join("data/testing/query_video", f))]
 
-vid_id = randint(0, 97)
-print("TESTING ", onlyfiles[vid_id])
+# vid_id = randint(0, 97)
+# print("TESTING ", onlyfiles[vid_id])
 
-sys.argv = ["", "data/testing/query_video/" + onlyfiles[vid_id][0] + ".mp4", "data/testing/query_audio/" + onlyfiles[vid_id][0] + ".wav"]
-assert len(sys.argv) == 3, f"Usage python <name>.py <query-video-path>.mp4 <query-audio-path>.wav"
-query_video_path = sys.argv[1]
-query_audio_path = sys.argv[2]
+for vid_path, media_id, start_time in onlyfiles:
+    query_video_path = "data/testing/query_video/" + vid_path + ".mp4"
+    query_audio_path = "data/testing/query_audio/" + vid_path + ".wav"
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+    input_data = dict(
+        id=uuid.uuid4().hex,
+        query_video_path=query_video_path,
+        query_audio_path=query_audio_path
+    )
 
-input_data = dict(
-    id=uuid.uuid4().hex,
-    query_video_path=query_video_path,
-    query_audio_path=query_audio_path
-)
+    socket.send(pickle.dumps(input_data))
 
-socket.send(pickle.dumps(input_data))
+    result = pickle.loads(socket.recv())
+    socket.close()
+    print(vid_path, result)
 
-result = pickle.loads(socket.recv())
-print(result)
